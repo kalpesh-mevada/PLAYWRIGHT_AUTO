4ba50d17cab9709e6499c41fa8d0ff73335bf1f5
Kalpesh Mevada <rock003kalpesh@gmail.com>
2026-07-25 14:50:07 +0530

    saucedemo login

--- tests/login.demo.spec.ts
+++ tests/login.demo.spec.ts
@@ -2,5 +2,11 @@ import {test, expect} from '@playwright/test';
 
 test( 'Login test', async ({page}) =>{
     await page.goto('https://www.saucedemo.com/');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page.locator('.title')).toHaveText('Products');
+    await expect(page.locator('.inventory_item')).toHaveCount(6);
     await page.pause();
 })
