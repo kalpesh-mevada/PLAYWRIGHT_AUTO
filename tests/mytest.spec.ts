4ae3fd04bb2fa6488d122db427470eb4533de842
Kalpesh Mevada <rock003kalpesh@gmail.com>
2026-07-25 14:40:07 +0530

    Initial Commit

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+test('', async ({page}) => {
+    await page.goto('https://www.google.com/');
+    await page.locator('//textarea[@id="APjFqb"]').fill('playwright testing')
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(2000); //wait for 2 seconds to allow search results to load
+    const result = await page.locator('#search').count();   
+    expect(result).toBeGreaterThan(0)
+    await page.pause() //pause the test to see the result in the browser
+})
